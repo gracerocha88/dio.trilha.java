d83627eac1952648d95c8aa83f57f150346a5c4e
gracerocha <gracerocha1602@gmail.com>
2023-03-27 17:25:52 -0300

    ex.loops

---
+++ loops/nomeIdade.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class nomeIdade{
+
+public static void main(String[]args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    String nome;
+    int idade;
+
+    System.out.println("nome:  ");
+nome = scan.next();
+
+    System.out.println("idade: ");
+    idade = scan.nextInt();
+}
+}
