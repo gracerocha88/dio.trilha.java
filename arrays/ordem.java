a19680c9f02fa24c61a7e69dacaf809ad20d070a
gracerocha <gracerocha1602@gmail.com>
2023-03-27 18:54:50 -0300

    exercicios loop e arrays.JAVA

---
+++ arrays/ordem.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class ordem {
+    public static void main(String[] args) {
+
+        int[] vetor = { -5, -6, 15, 50, 8, 4 };
+        System.out.println("vetor: ");
+        int count = 0;
+        while (count < vetor.length) {
+            System.out.print(vetor[count] + " ");
+            count++;
+
+        }
+        System.out.print("\nvetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
